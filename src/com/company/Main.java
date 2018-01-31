ffe2b9084d645d10272a5524e5cd94b1a1be57c7
Alex Keith <alexmkeith13@gmail.com>
2018-01-31 16:33:09 -0500

    Cat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Cat cat = new Cat(4,true, "Calico");
+
+        System.out.println(cat.getColor());
+        System.out.println(cat.getLegs());
+        System.out.println(cat.isFur());
         
     }
 }
